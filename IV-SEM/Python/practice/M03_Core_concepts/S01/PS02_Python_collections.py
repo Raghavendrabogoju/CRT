bf57e84ceaf3a6b79123fce1479265aa322977bc
Raghavendrabogoju <raghavendrabogoju@gmail.com>
2026-03-09 12:19:38 +0530

    Assignments

--- IV-SEM/Python/practice/M03_Core_concepts/S01/PS02_Python_collections.py
+++ IV-SEM/Python/practice/M03_Core_concepts/S01/PS02_Python_collections.py
@@ -1,11 +1,36 @@
 '''
 set topic :
 '''
-a=set([12,23,34,45,56,67])
+'''a=set([12,23,34,45,56,67])
 print(a)
 a.add(1234)
 a.add(34356)
 print(a)
 a.remove(12)
 a.discard(23)
-print(a)
+print(a)'''
+
+"""t1=(3,4,5,6,7,8,9 )
+t2=(1,2,3,4,5,6)
+print(t1,t2)
+"""
+''''
+dir -Force
+git init
+git add
+git commit -m "Assignments"
+git pull origin main
+git push origin main
+git status'''
+
+d={"name":"raghu",'age':"12"}
+print(d)
+print(d.keys())
+print(d.values())
+
+d={"name":"kalyani","age":24}
+d["name"]='sao paulo'
+print(d)
+d.pop('name')
+del d['age']
+print(d)
