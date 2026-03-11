1c7f3b7499b72dcbfb5e1d5b0c4e46ba61f16994
Raghavendrabogoju <raghavendrabogoju@gmail.com>
2026-03-11 12:26:24 +0530

    message commit

--- IV-SEM/Python/Assignments/M02_Logic_Building_and_Patterns/AST02/task.py
+++
@@ -1,9 +0,0 @@
-def reverse_number(n: int) -> int:
-    sign = -1 if n < 0 else 1
-    n = abs(n)
-    rev = int(str(n)[::-1])
-    return sign * rev
-
-if __name__ == "__main__":
-    n = int(input())
-    print(reverse_number(n))
--- IV-SEM/Python/Assignments/M02_Logic_Building_and_Patterns/AST03/task.py
+++
@@ -1,6 +0,0 @@
-def sum_of_digits(n: int) -> int:
-    return sum(int(d) for d in str(abs(n)))
-
-if __name__ == "__main__":
-    n = int(input())
-    print(sum_of_digits(n))
