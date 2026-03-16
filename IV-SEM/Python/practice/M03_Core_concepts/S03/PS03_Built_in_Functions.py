d4a83361f44656fb7bf92f1108a2d9737479bdf3
Raghavendrabogoju <raghavendrabogoju@gmail.com>
2026-03-16 11:31:17 +0530

    message commit

---
+++ IV-SEM/Python/practice/M03_Core_concepts/S03/PS03_Built_in_Functions.py
@@ -0,0 +1,15 @@
+'''
+1)Find the largest number (using(max))
+2)Check Palindrome (Using reversed() & join())
+3)Count Even Numbers (Using(filter()))
+4)Remove Duplicates( Using sum())
+5)Sum of DIgits (using(sum()))
+6) sort words alphabetically (using(sor()))
+7)find common elements(using(set()))
+8)index with calue (using Enumerate())
+9)pair two lists(using zip())
+10)find second largest number(using sorted())'''
+a=[1,1,2,2,3,3,34,4,5,5,75]
+print(set(a))
+b=["kalyani",'renu','raju']
+print(sorted(b))
